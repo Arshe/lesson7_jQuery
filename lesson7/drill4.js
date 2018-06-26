0e757e9e2e19519c1b3c0da3a2190fc009f85156
Brian Lorenz <ashrai@comcast.net>
2018-06-26 16:23:33 +0000

    drill trouble

---
+++ lesson7/drill4.js
@@ -0,0 +1,20 @@
+/*
+    Program Name: Recipe Display Application
+    Author: Brian Lorenz
+    Date: 06-25-2018
+    Filename: drill4.js
+    */
+//displays the next element after the current target
+/*function display(event) {
+    $(event.currentTarget).next().fadeIn("slow");
+} //end of display*/
+
+//attach event listener to h3 elements to invoke display function when clicked
+//$("h3").click(display);
+//displays and animates the next element after the current target
+function display2(event) {
+    
+    $(event.currentTarget).next().animate({ height: ‘toggle’ }, “slow”);
+} //end of display2
+//attach event listener to h3 elements to invoke display function when clicked
+$("h3").click(display2);
